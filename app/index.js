9f9b6c32f33d3a647cecee51b298042e1f4be880
Ryan Tablada <ryan.tablada@gmail.com>
2016-10-12 10:51:40 -0500

    Find the sum of all accounts

--- app/index.js
+++ app/index.js
@@ -1,1 +1,25 @@
 import 'whatwg-fetch';
+// Pull in people data from people.json file
+import people from './people';
+
+// Get the name of the third account
+console.log(people[2].name);
+
+// Create a new list to store acct balances
+var acctBalances = [];
+
+// Loop through all of our people
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+
+  acctBalances.push(parseInt(person.account_balance));
+}
+
+let sum = 0;
+
+for (let i = 0; i < acctBalances.length; i = i + 1) {
+  // console.log('i: ', i, 'balance: ', acctBalances[i], 'sum: ', sum);
+  sum = sum + acctBalances[i];
+}
+
+console.log(sum);
